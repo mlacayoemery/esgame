3679840a6aeef373bede81316158a24ef26ff31a
Nico Kappeler <53252190+kappni@users.noreply.github.com>
2023-04-11 13:23:55 +0200

    init TiffService

--- v2/src/app/services/tiff.service.ts
+++ v2/src/app/services/tiff.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { fromBlob, fromUrl, writeArrayBuffer } from 'geotiff';
+import { GameBoard } from '../shared/models/game-board';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,24 @@ import { Injectable } from '@angular/core';
 export class TiffService {
 
   constructor() { }
+
+  async tiffToArray(blob: Blob | null) : Promise<number[]> {
+    const tiff = await fromUrl("http://localhost:4200/assets/images/esgame_img_ag_hunt.tif"); //await fromBlob(blob);
+    const image = await tiff.getImage();
+
+    const raster = await image.readRasters({ interleave: true });
+    const result = Array.from(raster.map(c => c as number));
+    
+    return result;
+  }
+
+  async arrayToTiff(data: number[], columns: number) : Promise<Blob> {
+    const height = data.length / columns;
+    const buffer = writeArrayBuffer(data, { width: columns, height: height, GDAL_NODATA: "-1"}) as ArrayBufferLike;
+    const result = new Blob([buffer], { type: 'application/octet-stream' });
+    return result;
+  }
+
 }
+
+
