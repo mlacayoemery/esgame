304310c3fafb33d93c16f984b438e7d5e3a1721e
Martin Lacayo-Emery <mlacayoemery@users.noreply.github.com>
2023-06-12 16:03:01 +0200

    Added POST call with JSON for R example for hosting a process

--- tools/R/example.R
+++ tools/R/example.R
@@ -1,9 +1,17 @@
+library(jsonlite)
+
 #* @get /hello
 hello <- function() {
   print("Hello")
 }
 
-#* @post /hello
+#* @post /hellothere
 hellothere <- function(req, text ="World!") {
   print(paste("Hello ", text, sep = ""))
+}
+
+#* @post /hellojson
+hellojson <- function(req, jsontext ="[{\"who\" : \"World\"}]") {
+  jsontext = fromJSON(jsontext, simplifyVector= TRUE)
+  print(paste("Hello ", jsontext$who, sep = ""))
 }
