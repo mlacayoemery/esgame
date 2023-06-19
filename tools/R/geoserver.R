da4827267924bcf1484aff4a3ba3bb6d4b73e97b
Martin Lacayo-Emery <mlacayoemery@users.noreply.github.com>
2023-06-19 14:48:39 +0200

    Debugging R example script for GeoServer

--- tools/R/geoserver.R
+++ tools/R/geoserver.R
@@ -1,8 +1,14 @@
+install.packages('geosapi')
 library(geosapi)
 #see https://cran.r-project.org/web/packages/geosapi/vignettes/geosapi.html
 #cite https://doi.org/10.5281/zenodo.1184895
 
-gsman <- GSManager$new(
+#remove.packages("geosapi")
+#install.packages("C:/Users/Martin/Workspace/geosapi", 
+#                 repos = NULL, 
+#                 type = "source")
+
+gsman <-GSManager$new(
   url = "http://localhost:80/geoserver", #baseUrl of the Geoserver
   user = "admin", pwd = "geoserver", #credentials
   logger = NULL #logger, for info or debugging purpose
@@ -13,29 +19,29 @@ gsman <- GSManager$new(
 created <- gsman$createWorkspace("esgame", "https://esgame.unige.ch")
 
 #create new datastore
-# ds = GSShapefileDirectoryDataStore$new(dataStore="esgame_datastore",
-#                                        description = "esgame_datastore description",
-#                                        enabled = TRUE,
-#                                        url = "file:data/sf")
-# created <- gsman$createDataStore("risk", ds)
+ds = GSShapefileDirectoryDataStore$new(dataStore="esgame_datastore",
+                                      description = "esgame_datastore description",
+                                      enabled = TRUE,
+                                      url = "file:data/sf")
+created <- gsman$createDataStore("risk", ds)
 
-options(error = function() {
-  sink(stderr())
-  on.exit(sink(NULL))
-  traceback(3, max.lines = 1L)
-  if (!interactive()) {
-    q(status = 1)
-  }
-})
+#options(error = function() {
+#  sink(stderr())
+#  on.exit(sink(NULL))
+#  traceback(3, max.lines = 1L)
+#  if (!interactive()) {
+#    q(status = 1)
+#  }
+#})
 
 uploaded <- gsman$uploadShapefile(
   ws = "esgame", ds = "poi",
   endpoint = "file", configure = "first", update = "overwrite",
-  charset = "UTF-8", filename = "C:/Users/Martin/workspace/esgame/tools/R/poi.shp"
+  charset = "UTF-8", filename = "C:/Users/Martin/workspace/esgame/tools/R/poi.zip"
 )
 
-packageVersion("geosapi")
-packageVersion("httr")
+#packageVersion("geosapi")
+#packageVersion("httr")
 
 #library(packrat)
 #packrat:::recursivePackageDependencies("geosapi",lib.loc = .libPaths()[1])
